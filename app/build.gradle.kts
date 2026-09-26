068a54efd720e2bffce332800de9bb10e68f50d9
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-09-26 22:45:51 +1000

    flush

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.odiousapps.mx3buttonmapper"
         minSdk = 26
         targetSdk = 37
-        versionCode = 41
-        versionName = "0.0.41"
+        versionCode = 42
+        versionName = "0.0.42"
     }
 
     buildTypes {
